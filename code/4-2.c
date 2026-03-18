55d402d2c408150f3e4692fea4783ba186bfc7ba
gganghw <sunflower110714@gmail.com>
2026-03-18 14:35:14 +0000

    to 4-3 rererererere

--- code/4-2.c
+++ code/4-2.c
@@ -5,7 +5,7 @@ int main()
     for(int a = 1; a<10; a++){
         for(int i = 1; i<10; i++){
             int b = a*i;
-            printf(" %d * %d = %d\n", a, i, b);
+            printf("%d * %d = %d\n", a, i, b);
         }
     }
     return 0;
